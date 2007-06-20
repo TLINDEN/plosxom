2d7996d657ca2055eb45166ebc7143ec4e0b5aaa
palidhar <palidhar@96c5a40c-a532-0410-a9c3-699cfc0c3d3a>
2007-06-20 15:50:21 +0000

    added link list plugin

---
+++ plugins/links/links.php
@@ -0,0 +1,33 @@
+<?
+
+class links extends Plugin {
+  var $links;
+  function register() {
+    $this->smarty->register_function("links", array(&$this, "getlinks"));
+  }
+  
+  function getlinks($params, &$smarty) {
+    $tpl = '<li><a href="%1">%2</a></li>';
+    $cfg = 'links.conf';
+
+    if( $params["config"]) {
+      $cfg = $params["config"];
+    }
+
+    if( $params["template"] ) {
+      $tpl = $params["template"];
+    }
+    
+    $links = parse_config($cfg);
+
+    $out = "";
+    foreach ($links as $name => $link) {
+      $out .= str_replace(array("%1", "%2"), array($link, $name), $tpl); 
+    }
+
+    return $out;
+  }
+  
+}
+
+?>
