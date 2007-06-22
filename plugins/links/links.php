f5db37d036455e45624ea8ff8df4a40557cc145d
palidhar <palidhar@96c5a40c-a532-0410-a9c3-699cfc0c3d3a>
2007-06-22 15:52:43 +0000

    added docs to links and log module

--- plugins/links/links.php
+++ plugins/links/links.php
@@ -1,5 +1,77 @@
 <?
 
+ #
+ #   $Id$
+ #
+ #   By  accessing  this software,  Plosxom,  you are  duly informed
+ #   of and  agree to be  bound  by the  conditions  described below
+ #   in this notice:
+ #
+ #   This software product, Plosxom, is developed  by Pali Dhar  and
+ #   copyrighted (C) 2007  by  Pali Dhar,  with all rights reserved.
+ #
+ #   There is  no charge for  Plosxom software. You can redistribute
+ #   it and/or modify it under the terms of the Artistic License 2.0
+ #   which is incorporated by reference herein.
+ #
+ #   PLOSXOM IS PROVIDED "AS IS" AND WITHOUT  ANY EXPRESS OR IMPLIED
+ #   WARRANTIES,   INCLUDING,   WITHOUT  LIMITATION,   THE   IMPLIED 
+ #   WARRANTIES  OF  MERCHANTIBILITY  AND  FITNESS  FOR A PARTICULAR
+ #   PURPOSE.
+ #
+ #   You   should  have  received a  copy  of  the  artistic license
+ #   along with Plosxom.  You may download a copy of the license at:
+ #
+ #     http://dev.perl.org/licenses/artistic.html
+ #
+ #   Or contact:
+ #
+ #    "Pali Dhar" <nobrain@bk.ru>
+ #
+
+/*
+
+To install:
+
+1. copy links.php to your plugins directory.
+
+2. create one or more link configs in your etc directory. In your template
+   you can then loop over links by config. This makes it possible
+   to maintain page links separately from blogroll or the like.
+
+   Format is simple:
+
+     link name = link url
+
+   for example:
+
+     Peppos Incarnation = http://foo.bar/
+
+   Empty lines and lines starting with # will be ignored as
+   always in in configs.
+
+3. add some code to your template to loop over the links of
+   a particular links config file, eg:
+
+   <div class="links">
+     <h4>Blogroll</h4>
+     <ul>
+      
+      {links config="blogroll.conf" template="<a href='%1'>%2</a> "}
+    </ul>
+   </div>
+
+   The template function "links" exported by the links module
+   requires 2 parameters:
+
+    config   - the link config file you are referring to
+    template - how the link shall appear. %1 will be used for the
+               url and %2 for the link name.
+
+That's it - now add links to your link configs as you like!
+
+*/
+
 class links extends Plugin {
   var $links;
   function register() {
--- plugins/log/log.php
+++ plugins/log/log.php
@@ -1,4 +1,53 @@
 <?
+ #
+ #   $Id$
+ #
+ #   By  accessing  this software,  Plosxom,  you are  duly informed
+ #   of and  agree to be  bound  by the  conditions  described below
+ #   in this notice:
+ #
+ #   This software product, Plosxom, is developed  by Pali Dhar  and
+ #   copyrighted (C) 2007  by  Pali Dhar,  with all rights reserved.
+ #
+ #   There is  no charge for  Plosxom software. You can redistribute
+ #   it and/or modify it under the terms of the Artistic License 2.0
+ #   which is incorporated by reference herein.
+ #
+ #   PLOSXOM IS PROVIDED "AS IS" AND WITHOUT  ANY EXPRESS OR IMPLIED
+ #   WARRANTIES,   INCLUDING,   WITHOUT  LIMITATION,   THE   IMPLIED 
+ #   WARRANTIES  OF  MERCHANTIBILITY  AND  FITNESS  FOR A PARTICULAR
+ #   PURPOSE.
+ #
+ #   You   should  have  received a  copy  of  the  artistic license
+ #   along with Plosxom.  You may download a copy of the license at:
+ #
+ #     http://dev.perl.org/licenses/artistic.html
+ #
+ #   Or contact:
+ #
+ #    "Pali Dhar" <nobrain@bk.ru>
+ #
+
+/*
+
+To install:
+
+1. copy log.php to your plugins directory.
+
+2. copy log.conf to  your etc directory and configure it.
+   the config contains hints what can be changed. Most important
+   is "logdir", see below
+
+3. create the log directory (configured as "logdir" in log.conf,
+   make sure, the webserver has write access. If your user/group
+   membership differs from weberserver, add the setguid bit, eg:
+
+   % chmod 2777 logs/
+
+   This way, at least the group ownerships of files created there
+   in will be preserved so you can still maintain the logs.
+
+*/
 
 class log extends Plugin {
   function register() {
