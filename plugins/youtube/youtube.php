beee34de8db0cf8e1762c549c6f054e363477636
palidhar <palidhar@96c5a40c-a532-0410-a9c3-699cfc0c3d3a>
2007-08-02 13:07:51 +0000

    added google video support too and enhanced plugin documentation

--- plugins/youtube/youtube.php
+++ plugins/youtube/youtube.php
@@ -31,9 +31,10 @@
 /*
 
 This is the youtube plugin which allows you to
-post youtube videos to your blog by just entering
-the video id without the hassle of digging through
-youtube html source to find the player embed code.
+post youtube or google videos to your blog by just
+entering the video id without the hassle of digging
+through youtube html source to find the player embed
+code.
 
 This plugin requires version 1.06 of plosxom whose
 templates are using the smarty eval() function for
@@ -47,6 +48,20 @@ with
 
   {eval var=$post.text}
 
+The plugin generates code which is a little bit
+userfriendly than youtube and google video. In case
+of google video if you choose the preview image
+link mode (mode="image") the plugin connects to
+video.google.com to retrieve the prober image uri.
+If that fails, it falls back to ascii mode and just
+prints a link to the video - so don't wonder.
+
+You may of course change the provided template for
+the plugin, however make sure all if's and tweaks
+required by the plugin remain.
+
+To install:
+
 1. copy youtube.php to your plugins directory.
 
 2. copy youtube.tpl to your current template directory.
@@ -55,10 +70,15 @@ That's all about it.
 
 To include a youtube video in a blog post, do the following:
 
-- visit the youtube video page you want to post
-- retrieve the video id, example:
+- visit the youtube or google video page you want to post
+
+- retrieve the video id.
+  Youtube:
   http://youtube.com/watch?v=OEXt9qb0zBI
-                             ^^^^^^^^^^^  this is the video id
+                             ^^^^^^^^^^^
+  Google Video:
+  http://video.google.com/videoplay?docid=-8151365470057210599
+                                          ^^^^^^^^^^^^^^^^^^^^
 - in a new blog post add one of the following versions:
 
   o plain ascii html link to the youtube page:
@@ -89,12 +109,23 @@ class youtube extends Plugin {
     $tpl    = 'youtube.tpl';
     $width  = 450;
     $height = 370;
+    $yvideoid = "youtube";
     
     if( $params["id"]) {
       $id = $params["id"];
     }
     else {
       return;
+    }
+
+    if (ereg('^\-?[0-9]*$', $id)) {
+      $yvideoid = "google";
+    }
+
+    if ($yvideoid == "google") {
+      /* adjust defaults */
+      $width  = 400;
+      $height = 326;
     }
 
     if( $params["width"]) {
@@ -109,10 +140,25 @@ class youtube extends Plugin {
       $tpl = $params["template"];
     }
 
-    $this->smarty->assign("yid",     $id);
-    $this->smarty->assign("yheight", $height);
-    $this->smarty->assign("ywidth",  $width);
-    $this->smarty->assign("mode",    $params["mode"]);
+    if($yvideoid == "google" and $params["mode"] == "image") {
+      /* need to fetch the img uri, mad enough there's no api
+         to retrieve google video thumbnail images nor any way
+	 to create the thumbnail url from the video id */
+      $page = implode('', file("http://video.google.com/videoplay?docid=$id"));
+      if (preg_match("#(http://video.google.com/ThumbnailServer[^\"]*)\"#", $page, $match)) {
+	$this->smarty->assign("ygooglethumbnail", $match[1]);
+      }
+      else {
+	/* hm, didn't find the thumbnail uri, fall back to link mode then */
+	$params["mode"] = "ascii";
+      }
+    }
+
+    $this->smarty->assign("yid",      $id);
+    $this->smarty->assign("yheight",  $height);
+    $this->smarty->assign("ywidth",   $width);
+    $this->smarty->assign("mode",     $params["mode"]);
+    $this->smarty->assign("yvideoid", $yvideoid);
 
     $this->smarty->display($this->config["template"] . "/" . $tpl);
   }
--- plugins/youtube/youtube.tpl
+++ plugins/youtube/youtube.tpl
@@ -1,18 +1,55 @@
 {if $mode == "ascii"}
 
-  <!-- plain ascii link to original youtube page -->
-  <a href="http://youtube.com/watch?v={$yid}">movie</a>
+  {if $yvideoid == "google"}
+
+    <!-- plain ascii link to original google video page -->
+    <a href="http://video.google.com/videoplay?docid={$yid}">movie</a>
+
+  {else}
+
+    <!-- plain ascii link to original youtube page -->
+    <a href="http://youtube.com/watch?v={$yid}">movie</a>
+
+  {/if}
 
 {elseif $mode == "image"}
 
-  <!-- preview image with link to youtube page -->
-  <a href="http://youtube.com/watch?v={$yid}"><img
-     src="http://img.youtube.com/vi/{$yid}/default.jpg"
-     border="0" title="watch movie at youtube"></a>
+  {if $yvideoid == "google"}
+
+    <!-- preview image with link to google video page -->
+    <a href="http://video.google.com/videoplay?docid={$yid}"><img
+      src="{$ygooglethumbnail}" border="0" title="watch movie at google video"></a>
+
+  {else}
+    <!-- preview image with link to youtube page -->
+    <a href="http://youtube.com/watch?v={$yid}"><img
+       src="http://img.youtube.com/vi/{$yid}/default.jpg"
+       border="0" title="watch movie at youtube"></a>
+  {/if}
 
 {else}
 
-  <!--
+  <div class="youtube">
+
+  {if $yvideoid == "google"}
+
+    <!-- embed video player with google video -->
+    <object height="{$yheight}" width="{$ywidth}">
+      <param name="movie" value="http://video.google.com/googleplayer.swf?docId={$yid}"/>
+        <embed src="http://video.google.com/googleplayer.swf?docId={$yid}"
+          id="VideoPlayback" type="application/x-shockwave-flash"
+	  height="{$yheight}"
+	  width="{$ywidth}"
+          pluginspage="http://www.adobe.com/go/getflashplayer">
+        </embed>
+	<noembed>
+          <a href="http://video.google.com/videoplay?docid={$yid}">movie</a>
+        </noembed>
+    </object>
+
+  {else}
+
+    <!--
        embed video player with youtube video.
        please note, that we are a little bit nicer
        to our users than youtube.com, we are providing
@@ -21,21 +58,23 @@
        no internet standard at all!) and a link to the
        youtube page if a browser doesn't support the
        embed html tag, if any.
-   -->
- <div class="youtube">
-  <object height="{$yheight}" width="{$ywidth}">
-   <param name="movie" value="http://www.youtube.com/v/{$yid}">
-    <embed src="http://www.youtube.com/v/{$yid}"
+    -->
+    <object height="{$yheight}" width="{$ywidth}">
+     <param name="movie" value="http://www.youtube.com/v/{$yid}"/>
+      <embed src="http://www.youtube.com/v/{$yid}"
            type="application/x-shockwave-flash"
 	   quality="high"
 	   allowfullscreen="true"
 	   height="{$yheight}"
-	   pluginspage="http://www.adobe.com/go/getflashplayer"
-	   width="{$ywidth}">
-    <noembed>
-     <a href="http://youtube.com/watch?v={$yid}">movie</a>
-    </noembed>
-  </object>
- </div>
+	   width="{$ywidth}"
+	   pluginspage="http://www.adobe.com/go/getflashplayer">
+      <noembed>
+       <a href="http://youtube.com/watch?v={$yid}">movie</a>
+      </noembed>
+    </object>
+
+  {/if}
+
+  </div>
 
 {/if}
