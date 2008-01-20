845be1e66133e1ebb8686251685464bf3f286e27
palidhar <palidhar@96c5a40c-a532-0410-a9c3-699cfc0c3d3a>
2008-01-20 00:02:20 +0000

    added index

--- plugins/page/plugin_admin_page.php
+++ plugins/page/plugin_admin_page.php
@@ -12,7 +12,33 @@ class plugin_admin_page extends Plugin{
     }
   }
 
-  function admin_page() {}
+  function admin_page() {
+    $pages = $this->fetchall();
+    $this->smarty->assign('pages', $pages);
+  }
+
+  function fetchall() {
+    $pages = array();
+    if(file_exists($this->pconfig["data"]) and is_dir($this->pconfig["data"])) {
+      $dh = opendir($this->pconfig["data"]);
+      while ( $file = readdir($dh) ) {
+        if($file == "." or $file == "..") { continue; }
+	if(is_readable($this->pconfig["data"] . "/" . $file)) {
+          $entry = standard::getfile($this->pconfig["data"], $file, "", true);
+	  $pages[] = $entry;
+	}
+      }
+      $sort = array();
+      foreach ($pages as $pos => $entry) {
+        $sort[$pos] = $entry["mtime"];
+      }
+      array_multisort($sort, SORT_DESC, $pages);
+    }
+    else {
+      $this->smarty->assign("admin_error", "data path for static pages does not exist");
+    }
+    return $pages;
+  }
 }
 
 ?>
