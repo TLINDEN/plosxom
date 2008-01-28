e82ebd9488ec325cc2be13c7f495e1b660fd91f7
palidhar <palidhar@96c5a40c-a532-0410-a9c3-699cfc0c3d3a>
2008-01-28 00:12:14 +0000

    added spport to omit css class blogparagraph when adding <p> paragraphs after reading a post (required by admin-plugin visual editor, which only supports plain <p> tags without any css)

--- src/plugins/standard.php
+++ src/plugins/standard.php
@@ -1,39 +1,39 @@
 <?
- #
- #   $Id$
- #
- #   This  file  is  part of the  Plosxom  web-based authoring tool.
- #
- #   By  accessing  this software,  Plosxom,  you are  duly informed
- #   of and  agree to be  bound  by the  conditions  described below
- #   in this notice:
- #
- #   This software product, Plosxom, is developed  by Pali Dhar  and
- #   copyrighted (C) 2007  by  Pali Dhar,  with all rights reserved.
- #
- #   There is  no charge for  Plosxom software. You can redistribute
- #   it and/or modify it under the terms of the Artistic License 2.0
- #   which is incorporated by reference herein.
- #
- #   PLOSXOM IS PROVIDED "AS IS" AND WITHOUT  ANY EXPRESS OR IMPLIED
- #   WARRANTIES,   INCLUDING,   WITHOUT  LIMITATION,   THE   IMPLIED 
- #   WARRANTIES  OF  MERCHANTIBILITY  AND  FITNESS  FOR A PARTICULAR
- #   PURPOSE.
- #
- #   You   should  have  received a  copy  of  the  artistic license
- #   along with Plosxom.  You may download a copy of the license at:
- #
- #     http://dev.perl.org/licenses/artistic.html
- #
- #   Or contact:
- #
- #    "Pali Dhar" <nobrain@bk.ru>
- #
+ //
+ //   $Id$
+ //
+ //   This  file  is  part of the  Plosxom  web-based authoring tool.
+ //
+ //   By  accessing  this software,  Plosxom,  you are  duly informed
+ //   of and  agree to be  bound  by the  conditions  described below
+ //   in this notice:
+ //
+ //   This software product, Plosxom, is developed  by Pali Dhar  and
+ //   copyrighted (C) 2007  by  Pali Dhar,  with all rights reserved.
+ //
+ //   There is  no charge for  Plosxom software. You can redistribute
+ //   it and/or modify it under the terms of the Artistic License 2.0
+ //   which is incorporated by reference herein.
+ //
+ //   PLOSXOM IS PROVIDED "AS IS" AND WITHOUT  ANY EXPRESS OR IMPLIED
+ //   WARRANTIES,   INCLUDING,   WITHOUT  LIMITATION,   THE   IMPLIED 
+ //   WARRANTIES  OF  MERCHANTIBILITY  AND  FITNESS  FOR A PARTICULAR
+ //   PURPOSE.
+ //
+ //   You   should  have  received a  copy  of  the  artistic license
+ //   along with Plosxom.  You may download a copy of the license at:
+ //
+ //     http://dev.perl.org/licenses/artistic.html
+ //
+ //   Or contact:
+ //
+ //    "Pali Dhar" <nobrain@bk.ru>
+ //
 
- #
- # this is the standard plugin of plosxom.
- # it is required for most actions, so don't de-install it!
- #
+ //
+ // this is the standard plugin of plosxom.
+ // it is required for most actions, so don't de-install it!
+ //
 
 
 class standard extends Plugin {
@@ -60,8 +60,8 @@ class standard extends Plugin {
   }
 
   function hook_url_filter($path) {
-    #
-    # we look for category and archive
+    //
+    // we look for category and archive
     if(preg_match("/^\/category\/([a-zA-Z0-9\_\-]*)$/", $path, $match)) {
       $this->category = $match[1];
       $this->smarty->assign("category", $this->category);
@@ -101,7 +101,7 @@ class standard extends Plugin {
   }
 
   function fetch_dir($dir) {
-    # traverse a subirectory, aka category
+    // traverse a subirectory, aka category
     $dh = opendir($this->config["data_path"] . "/" . $dir);
     while ( $file = readdir($dh) ) {
       if($file != "." and $file != "..") {
@@ -112,7 +112,7 @@ class standard extends Plugin {
 
 
   function fetch_categories() {
-    # return a list of all directories (categories)
+    // return a list of all directories (categories)
     $dirs = array();
     $dh = opendir($this->config["data_path"]);
     while ( $file = readdir($dh) ) {
@@ -127,18 +127,18 @@ class standard extends Plugin {
 
 
   function fetch_file($file, $dir="") {
-    # fetch info about file (not the content of, this
-    # will be done later, after sorting of it)
+    // fetch info about file (not the content of, this
+    // will be done later, after sorting of it)
     $entry = $this->getfile($this->config["data_path"], $file, $dir, false);
     if($entry) {
       if( $this->archive ) {
         if( $entry["htime"] == $this->archivestamp ) {
-          # add it to the array, we'll sort it recursively later
+          // add it to the array, we'll sort it recursively later
           $this->files[] = $entry;
 	}
       }
       else if($this->archivelist) {
-        # we store the unix timestamp of 00:00:00 of the posting date
+        // we store the unix timestamp of 00:00:00 of the posting date
         $this->archivedates[mktime(0, 0, 0, date("m", $entry["mtime"]),
 	                    date("d", $entry["mtime"]), date("Y", $entry["mtime"]))] = 1;
       }
@@ -149,30 +149,31 @@ class standard extends Plugin {
   }
 
   function hook_storage_fetch($category, $id) {
-    #
-    # fetch a single posting
+    // fetch a single posting
     $post = $this->getfile($this->config["data_path"], $id . '.txt', $category);
     return ($post);
   }
 
-  function hook_storage_fetchall() {
-    #
-    # traverse directory tree and read all files therein
-    # matching certain criteria (category, archive, none)
-    # returns an array ready for smarty production
-    if(file_exists($this->config["data_path"])) {
+function hook_storage_fetchall($unlimited = false) {
+  /*
+   traverse directory tree and read all files therein
+   matching certain criteria (category, archive, none)
+   returns an array ready for smarty production
+  */
+  if(file_exists($this->config["data_path"])) {
+      $this->files = array();
       $dh = opendir($this->config["data_path"]);
       while ( $file = readdir($dh) ) {
 	if($file == "." or $file == "..") { continue; }
 	if(is_dir($this->config["data_path"] . "/" . $file) and is_readable($this->config["data_path"] . "/" . $file)) {
 	  if($this->category) {
-	    # category filter in use
+	    // category filter in use
 	    if( $this->category == $file ) {
 	      $this->fetch_dir($file);
 	    }
 	  }
 	  else {
-	    # no filter in use, fetch anyway
+	    // no filter in use, fetch anyway
 	    $this->fetch_dir($file);
 	  }
 	}
@@ -193,15 +194,15 @@ class standard extends Plugin {
 	return array();
       }
       else {
-        # so reverse sort the files
+        // so reverse sort the files
         $sort = array();
         foreach ($this->files as $pos => $entry) {
           $sort[$pos] = $entry["mtime"];
         }
         array_multisort($sort, SORT_DESC, $this->files);
 
-        # now look if there is a content filter registered
-        # if yes, then let it now filter the content
+        // now look if there is a content filter registered
+        // if yes, then let it now filter the content
         if($this->filter_plugin and !$this->category and !$this->archivelist and !$this->archive) {
           return $this->filter_plugin->hook_content_filter($this->files);
         }
@@ -210,7 +211,12 @@ class standard extends Plugin {
         $lastdate = 0;
 	$numfiles = count($this->files);
 	$this->input["numfiles"] = $numfiles;
-	$maxfiles = $this->input["past"] + $this->config["postings"];
+	if($unlimited) {
+	  $maxfiles = $numfiles;
+	}
+	else {
+	  $maxfiles = $this->input["past"] + $this->config["postings"];
+	}
         for($pos = $this->input["past"]; $pos < $maxfiles; $pos++) {
           if(array_key_exists($pos, $this->files)) {
             $entry = $this->getfile($this->config["data_path"], $this->files[$pos]["file"], $this->files[$pos]["category"]);
@@ -224,7 +230,7 @@ class standard extends Plugin {
 					   date("d", $this->files[$pos]["mtime"]),
 					   date("Y", $this->files[$pos]["mtime"]));
 	   if($blogdate != $lastdate) {
-	      # only save blogdate, if it has changed prior to previous posts
+	      // only save blogdate, if it has changed prior to previous posts
 	      $this->files[$pos]["blogdate"] = $blogdate;
 	      $lastdate = $blogdate;
 	    }
@@ -244,16 +250,16 @@ class standard extends Plugin {
   }
 
 
-  function getfile($datadir, $file, $dir="", $read=true) {
-    #
-    # actual open and read the file content and
-    # place it into an array
-    # if $read == true(default), the file content will be read.
-    # set it to true to save runtime if you only need file attributes
+  function getfile($datadir, $file, $dir="", $read=true, $paragraph=true) {
+    //
+    // actual open and read the file content and
+    // place it into an array
+    // if $read == true(default), the file content will be read.
+    // set it to true to save runtime if you only need file attributes
     $filename = $datadir . "/" . $dir . "/" . $file;
     $entry = array();
     if(is_readable($filename) and ereg('\.txt$', $file)) {
-      # dont stat() the file if this has already been done
+      // dont stat() the file if this has already been done
       $mtime         = filemtime($filename);
       $human_mtime   = date("Ymd", $mtime);
       $id            = preg_replace("/\.txt$/", "", $file);
@@ -267,18 +273,18 @@ class standard extends Plugin {
       );
   
       if($read) {
-         # also read the file content
-         # circumvent reading file content twice
+         // also read the file content
+         // circumvent reading file content twice
          $lines          = file($filename);
          $entry["title"] = trim(array_shift($lines));
-         $entry["text"]  = paragraph(implode('', $lines));
+         $entry["text"]  = paragraph(implode('', $lines), $paragraph);
          $entry["raw"]   = implode('', $lines);
       }
   
       return $entry;
     }
     else {
-      #print "$datadir/$dir/$file.txt is not readable\n";
+      //print "$datadir/$dir/$file.txt is not readable\n";
       report_error("$datadir/$dir/$file.txt is not readable");
       return null;
     }
@@ -286,7 +292,10 @@ class standard extends Plugin {
 }
 
 
-function paragraph(&$text) {
-  $text = preg_replace("/(\r\n\r\n|\n\n)/", "</p><p class=\"blogparagraph\">", $text);
+function paragraph(&$text, $paragraph=true) {
+  if($paragraph) {
+    $paragraph = " class='blogparagraph'";
+  }
+  $text = preg_replace("/(\r\n\r\n|\n\n)/", "</p><p$paragraph>", $text);
   return '<p class="blogparagraph">' . $text . '</p>';
 }
