d3818c92d94c02a10121d3fd0ea58e56f8e21b26
palidhar <palidhar@96c5a40c-a532-0410-a9c3-699cfc0c3d3a>
2008-01-28 00:19:06 +0000

    - added post_save support (rpc ping issued after saving a posting) - added own config file with listing of rpc sites

--- plugins/admin_rpc/plugin_admin_rpc.php
+++ plugins/admin_rpc/plugin_admin_rpc.php
@@ -5,15 +5,25 @@ class plugin_admin_rpc extends Plugin{
 
   function register() {
     $this->add_handler("admin_rpcping", "plugin_admin_rpc");
+    $this->add_handler("admin_postsave", "plugin_admin_rpc");
     $this->add_template("extra", "shared/extra_rpc.tpl");
+    $this->add_template("postsave", "shared/postsave_rpc.tpl");
     $this->smarty->assign("plugin_admin_rpcping", true);
     $this->options = parse_config("rpcsites.conf");
   }
 
-  function admin_rpcping() {
+  function admin_postsave() {
+    if($this->input['rpcping']) {
+      $this->admin_rpcping(true);
+    }
+  }
+
+  function admin_rpcping($return = false) {
     include_once($this->config["lib_path"] . "/IXR_Library.inc.php");
 
-    $this->smarty->assign("admin_mode", "admin_extras");
+    if (! $return) {
+      $this->smarty->assign("admin_mode", "admin_extras");
+    }
 
     if($this->options['rpcsite']) {
       if(! is_array($this->options['rpcsite']) ) {
@@ -21,11 +31,11 @@ class plugin_admin_rpc extends Plugin{
       }
     }
     else {
-      $this->smarty->assign("admin_error", "no rpc sites defined in rpcsites.conf!");
+      $this->smarty->append("admin_error", "no rpc sites defined in rpcsites.conf!");
       return;
     }
 
-    $info = 'Result:<br/>';
+    $info = '<br/>RPC Ping Result:<br/>';
     foreach ( $this->options['rpcsite'] as $url ) {
       $client = new IXR_Client($url);
       $client->query('weblogUpdates.ping', $this->config['blog_name'] . ' - ' . $this->config['blog_title'], $this->config['whoami']);
@@ -37,7 +47,10 @@ class plugin_admin_rpc extends Plugin{
 	$info .= "RPC Ping $url successfull.<br/>";
       }
     }
-    $this->smarty->assign("admin_msg", $info);
+    
+    $oldinfo = $this->smarty->get_template_vars('admin_info');
+    $this->smarty->append("admin_info", $oldinfo . $info);
+
   }
 
 }
