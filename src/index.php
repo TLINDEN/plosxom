a5faa286a9417f867114c09cf334c7c21998a2cd
palidhar <palidhar@96c5a40c-a532-0410-a9c3-699cfc0c3d3a>
2008-02-09 16:01:45 +0000

    added simple wrapper

---
+++ src/index.php
@@ -0,0 +1,42 @@
+<?
+/*
+ *
+ *   $Id: plosxom.php 72 2007-06-20 10:46:31Z palidhar $
+ *
+ *   This  file  is  part of the  Plosxom  web-based authoring tool.
+ *
+ *   By  accessing  this software,  Plosxom,  you are  duly informed
+ *   of and  agree to be  bound  by the  conditions  described below
+ *   in this notice:
+ *
+ *   This software product, Plosxom, is developed  by Pali Dhar  and
+ *   copyrighted (C) 2007  by  Pali Dhar,  with all rights reserved.
+ *
+ *   There is  no charge for  Plosxom software. You can redistribute
+ *   it and/or modify it under the terms of the Artistic License 2.0
+ *   which is incorporated by reference herein.
+ *
+ *   PLOSXOM IS PROVIDED "AS IS" AND WITHOUT  ANY EXPRESS OR IMPLIED
+ *   WARRANTIES,   INCLUDING,   WITHOUT  LIMITATION,   THE   IMPLIED 
+ *   WARRANTIES  OF  MERCHANTIBILITY  AND  FITNESS  FOR A PARTICULAR
+ *   PURPOSE.
+ *
+ *   You   should  have  received a  copy  of  the  artistic license
+ *   along with Plosxom.  You may download a copy of the license at:
+ *
+ *     http://dev.perl.org/licenses/artistic.html
+ *
+ *   Or contact:
+ *
+ *    "Pali Dhar" <nobrain@bk.ru>
+ *
+ */
+
+
+/*
+ * This file exists just for convenience so that users
+ * can access plosxom using the path notation
+ */
+
+include("plosxom.php");
+?>
